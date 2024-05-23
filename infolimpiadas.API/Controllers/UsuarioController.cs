173c68c9e36f2f16296d093d3e160e6693e1ffde
Matheus Gabriel <ms221103@gmail.com>
2024-05-23 13:29:34 -0300

    Mudanças no controller de login.

--- infolimpiadas.API/Controllers/UsuarioController.cs
+++ infolimpiadas.API/Controllers/UsuarioController.cs
@@ -33,6 +33,17 @@ namespace infolimpiadas.API.Controllers
             return Unauthorized();
         }
 
+        [HttpPost]
+        [Route("VerificarToken")]
+        public IActionResult ValidataToken([FromBody] string token)
+        {
+            bool valid = ValidateToken(token);
+            if (valid)
+                return Ok(valid);
+
+            return Unauthorized();
+        }
+
         private string GenerateJwtToken(string username)
         {
             var calims = new[]
@@ -53,7 +64,30 @@ namespace infolimpiadas.API.Controllers
                 signingCredentials: credentials
                 );
 
+            
             return new JwtSecurityTokenHandler().WriteToken(token); 
+        }
+
+        private bool ValidateToken(string token)
+        {
+            var handler = new JwtSecurityTokenHandler();
+            var parameters = new TokenValidationParameters
+            {
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ValidateLifetime = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"])),
+            };
+
+            try
+            {
+                handler.ValidateToken(token, parameters, out SecurityToken validatedToken);
+                return true;
+            }
+            catch (SecurityTokenException)
+            {
+                return false;
+            }
         }
     }
 }
